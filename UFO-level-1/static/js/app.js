be24826683fbe85d520ec5637173f3845ea460ec
Margaret Thorpe <mthorpester@gmail.com>
2021-03-29 13:22:49 -0700

    UFO reports displaying in table.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,16 @@
-// from data.js
-var tableData = data;
+// Get the UFO report data from data.js
+var UFOdata = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// For each UFO report, add a row to the table
+// For each row, go through each key-value pair in the corresponding UFOreport object and create a table cell for each value 
+
+data.forEach((UFOreport) => {
+  var row = tbody.append("tr");
+  Object.entries(UFOreport).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
